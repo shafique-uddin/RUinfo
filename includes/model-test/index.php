6b6f8a42f6bca57ce5397a53e1ddb5c2933a03c4
shafique-uddin <shofique32@gmail.com>
2023-03-25 12:36:51 +0600

    updated

--- includes/model-test/index.php
+++ includes/model-test/index.php
@@ -10,18 +10,12 @@
 
 <?php 
 $result = apply_filters('all_model_test_data_is_here', 'some data is here');
-
-
-
 foreach ($result as $key => $value) { ?>
-
-
-
 <div class="row">
     <div class="col-4"><?php echo $value->subjectName; ?></div>
     <div class="col-1"><?php echo $value->paperNo; ?></div>
     <div class="col-3"><?php echo $value->topicOrChapter; ?></div>
-    <div class="col-1"><?php echo $value->modelTestStatus; ?></div>
+    <div class="col-1"><?php if($value->modelTestStatus == '1'){echo 'On'; } else{ echo 'Off';} ?></div>
     <div class="col-1"><a href="">Edit</a></div>
     <div class="col-1"><a href="">View</a></div>
     <div class="col-1"><a href="">Delete</a></div>
