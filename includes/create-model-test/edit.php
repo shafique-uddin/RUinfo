742ab115470c5e99b7d1b22f2d47d33abc8fe201
shafique-uddin <shofique32@gmail.com>
2023-06-11 17:46:32 +0600

    updated

--- includes/create-model-test/edit.php
+++ includes/create-model-test/edit.php
@@ -1,46 +1,75 @@
-<div class="ruinfo_wrapper">
+<div class="ruinfo_wrapper question_edit">
+  <div class="modelTestTitle mb-3 text-center">
+    <div class="row">
+      <div class="col"><h2>Model Test Name: Bangla</h2></div>
+    </div>
+    <div class="row">
+      <div class="col"><h3>Chapter Name: </h3></div>
+    </div>
+    <div class="row">
+      <div class="col"><h5>Time: 10 Minutes</h5></div>
+    </div>
+  </div>
 
-<table class="table table-striped">
+<form action="">
+<table class="table table-striped">    
 
-    <tr>
-      <th colspan="4">1. Question</th>
-    </tr>
-    <tr>
-      <td>
-        A. Optons 1<br>
-        B. Optons 2<br>
-        C. Optons 3<br>
-        D. Optons 4
-      </td>
-    </tr>
-
-
-    <tr>
-      <th colspan="4">1. Question</th>
-    </tr>
-    <tr>
-      <td>
-        A. Optons 1<br>
-        B. Optons 2<br>
-        C. Optons 3<br>
-        D. Optons 4
-      </td>
-    </tr>
-
+      <tr>
+        <th colspan="4">1. <input type="text" name="question_no_" id="" value="sample question no 1"></th>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" name="question_no_1_option_no_1" id="" value="sample Option no 1"><br>
+          <input type="text" name="question_no_1_option_no_2" id="" value="sample Option no 2"><br>
+          <input type="text" name="question_no_1_option_no_3" id="" value="sample Option no 3"><br>
+          <input type="text" name="question_no_1_option_no_4" id="" value="sample Option no 4">
+        </td>
+      </tr>
 
 
-    <tr>
-      <th colspan="4">1. Question</th>
-    </tr>
-    <tr>
-      <td>
-        A. Optons 1<br>
-        B. Optons 2<br>
-        C. Optons 3<br>
-        D. Optons 4
-      </td>
-    </tr>
+      <tr>
+        <th colspan="4">2. <input type="text" name="question_no_" id="" value="sample question no 1" width="100%!important"></th>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" name="question_no_1_option_no_1" id="" value="sample Option no 1"><br>
+          <input type="text" name="question_no_1_option_no_2" id="" value="sample Option no 2"><br>
+          <input type="text" name="question_no_1_option_no_3" id="" value="sample Option no 3"><br>
+          <input type="text" name="question_no_1_option_no_4" id="" value="sample Option no 4">
+        </td>
+      </tr>
+
+
+
+      <tr>
+        <th colspan="4">3. <input type="text" name="question_no_" id="" value="sample question no 1" width="100%!important"></th>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" name="question_no_1_option_no_1" id="" value="sample Option no 1"><br>
+          <input type="text" name="question_no_1_option_no_2" id="" value="sample Option no 2"><br>
+          <input type="text" name="question_no_1_option_no_3" id="" value="sample Option no 3"><br>
+          <input type="text" name="question_no_1_option_no_4" id="" value="sample Option no 4">
+        </td>
+      </tr>
+
+
+      <!-- <tr>
+        <th colspan="4">1. Question</th>
+      </tr>
+      <tr>
+        <td>
+          A. Optons 1<br>
+          B. Optons 2<br>
+          C. Optons 3<br>
+          D. Optons 4
+        </td>
+      </tr> -->
+      <tr>
+        <td colspan="2"><input type="submit" value="Update"></td>
+      </tr>
 
 </table>
+</form>
 
 </div>
